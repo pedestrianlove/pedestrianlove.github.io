4d854615821a27d8697140ef1a26a2e1267d5513
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2023-05-12 16:21:11 +0800

    Update blog.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,36 @@
 <script>
     import "../css/app.css";
+    import Github_Icon from "../assets/images/github/github-mark-white.svg"
 </script>
 
-<header class="w-full h-10 bg-gray-700">
-    <nav class="grid-col-2 grid">
-        <div class="flex">
-            <a class="text-3xl" href="/">Surge and Sigh</a>
-        </div>
-        <div class="flex">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-        </div>
-    </nav>
-</header>
+<div class="flex flex-col h-screen justify-between">
+    <header class="w-full h-auto py-3 bg-gray-700 text-white">
+        <nav class="grid-cols-3 grid">
+            <div>
+                <a class="text-3xl font-mono" href="/">The Blog</a>
+            </div>
+            <div>
+                <a class="text-xl px-5 h-20" href="/">Home</a>
+                <a class="text-xl px-5 h-20" href="/blog">Blog</a>
+                <a class="text-xl px-5 h-20" href="/about">About</a>
+            </div>
+            <div>
+                <div class="items-end justify-items-end">
+                    <a class="right-0" href="https://github.com/pedestrianlove/">
+                        <img src={Github_Icon} alt="github-logo" class="h-10 w-auto">
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
 
-<slot />
+    <main class="mb-auto h-full">
+        <slot />
+    </main>
 
-<footer>
-    jsli@linux.com
-</footer>
+    <footer class="w-full h-auto bg-gray-700">
+        <div class="text-white text-center font-mono text-xl ">
+            Email: jsli@linux.com
+        </div>
+    </footer>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+import mdsvex from "mdsvex";
+import mdsvexConfig from "./mdsvex.config.js"
+//import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	"extensions": [".svelte", ...mdsvexConfig.extensions],
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	},
-	preprocess: vitePreprocess()
+	preprocess: vitePreprocess(),
+	prerender: {
+		crawl: true,
+		enabled: true,
+		onError: 'continue',
+		default: true
+	},
 };
 
 export default config;
